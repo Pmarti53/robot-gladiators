8474fb90f6e14292aa9d9b8f4fe69c43012f3735
pmarti53 <ptmartinezsound@gmail.com>
2021-02-08 11:53:51 -0600

    javascript functions to communicate with player

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,22 @@
-window.alert("this is an alert! JavaScript is running!")
+var playerName = window.prompt("what is your robot's name?");
+
+//What is this?
+
+console.log(playerName);
+
+
+console.log("this logs a string, good for leaving yourself a message")
+
+//this will do math and log 20
+
+console.log(10+10)
+
+//what is this?
+
+console.log("our robot's name is " + playerName);
+
+function fight() {
+    window.alert("the fight has begun")
+}
+
+//fight();
