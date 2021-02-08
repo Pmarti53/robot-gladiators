8534e2116dba377b8a3376938cb8cb5ed0e748e4
pmarti53 <ptmartinezsound@gmail.com>
2021-02-08 14:29:13 -0600

    conditions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,22 +1,48 @@
+// var playerName = 'T-900';
 var playerName = window.prompt("what is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-//What is this?
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log(playerName);
+// fight function
+var fight = function () {
+    // Alert player's that they are starting the round
+    window.alert("welcome to Robot Gladiators!");
 
+    
+    //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
+enemyHealth - playerAttack
 
-console.log("this logs a string, good for leaving yourself a message")
+    //Log a resulting message to the console so that we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + "health remaining "
+    );
 
-//this will do math and log 20
+    // check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left. ");
+    }
 
-console.log(10+10)
+    console.log(
+        enemyName + "attacked" + playerName + ". " + playerName + " now has " + playerHealth + "health remaining" 
+    );
+    // check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died ");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 
-//what is this?
+    //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
 
-console.log("our robot's name is " + playerName);
+    //Log a resulting message to the console so we know that it worked.
+};
 
-function fight() {
-    window.alert("the fight has begun")
-}
-
-//fight();
+fight();
